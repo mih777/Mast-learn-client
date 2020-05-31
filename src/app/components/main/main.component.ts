48a5bdc165375e26bb23cc824c53d8dbc54ece93
mih777 <mih777777@yandex.ru>
2020-05-31 16:53:04 +0300

    start pagination adding

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, SimpleChanges, OnChanges, DoCheck } from '@angular/core';
 import { TodoServiceService } from 'src/app/services/todo-service.service';
 import { Todo } from '../../interfaces'
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-main',
@@ -9,8 +10,7 @@ import { Todo } from '../../interfaces'
 })
 export class MainComponent implements
   OnInit,
-  OnChanges,
-  DoCheck 
+  OnChanges 
 {
 
   arrayId = []
@@ -18,6 +18,17 @@ export class MainComponent implements
   catName = this.todoService.catName
   loading: boolean = false
   quantity: number
+
+  
+  oSub: Subscription
+  oSub2: Subscription
+  oSub3: Subscription
+
+  pagination = 3
+  page: number = 1
+  limit_array = false
+
+  pages = []
 
   constructor(
     private todoService: TodoServiceService
@@ -28,13 +39,63 @@ export class MainComponent implements
   }
 
   ngOnInit(): void {
+
+    this.oSub2 = this.todoService.get_all_noparams()
+      .subscribe(res => {
+
+        // Создаем массив колличества страниц
+        for(let i = 1; i <= Math.ceil(res.length / this.pagination); i++){
+          this.pages[i] = i
+        }
+
+        this.todoService.catName == 'all' ? this.fetchAllTodos() : this.fetchTodosByCategory(this.todoService.catName)
+      }) 
     
-    this.todoService.catName == 'all' ? this.fetchAllTodos() : this.fetchTodosByCategory(this.todoService.catName)
-    console.log('ngOnInit')
+    
+    
   }
 
-  ngDoCheck(){
+  click_number_page(page){
+    this.page = page 
+    console.log(this.page)
+    this.todoService.catName == 'all' ? this.fetchAllTodos() : this.fetchTodosByCategory(this.todoService.catName)
+  }
 
+  to_begin(){
+    this.page = 1 
+    console.log(this.page)
+    this.todoService.catName == 'all' ? this.fetchAllTodos() : this.fetchTodosByCategory(this.todoService.catName)  
+  }
+
+  to_end(){
+    this.oSub3 = this.todoService.get_all_noparams()
+      .subscribe(res => {
+        this.page = Math.ceil(res.length/this.pagination)
+        this.todoService.catName == 'all' ? this.fetchAllTodos() : this.fetchTodosByCategory(this.todoService.catName)
+      })
+  }
+
+  decrise(){
+    
+    if(this.page === 1){
+      return
+    } else{
+      this.limit_array = false
+      this.page -=1
+      this.todoService.catName == 'all' ? this.fetchAllTodos() : this.fetchTodosByCategory(this.todoService.catName)
+    }
+    
+    
+  }
+
+  incrise(){
+    
+    if(this.page === 0 || this.limit_array){
+      return
+    }else{
+      this.page +=1
+      this.todoService.catName == 'all' ? this.fetchAllTodos() : this.fetchTodosByCategory(this.todoService.catName)
+    }
   }
 
   inpSelect(event){
@@ -51,13 +112,21 @@ export class MainComponent implements
       })
   }
 
+
   fetchAllTodos(){
     this.loading = true
-    this.todoService.getAll()
-      .subscribe(result => {
-        this.quantity = result.length
-        this.todos = result
-        this.loading = false
+    this.oSub = this.todoService.get_all(this.pagination, this.page)
+      .subscribe(res => {
+        
+        if(res.length === 0){
+          this.limit_array = true
+          return
+        } else{
+          this.quantity = res.length
+          this.todos = res
+          this.loading = false
+        }
+        
       })
   }
 
--- src/app/services/todo-service.service.ts
+++ src/app/services/todo-service.service.ts
@@ -11,48 +11,58 @@ export class TodoServiceService {
 
   catName: string = 'all'
   currentId: string 
+  //url = 'https://mast-learn-server.herokuapp.com'
+  url = 'http://localhost:3000'
 
   constructor(private http: HttpClient) { }
 
   create(todo: Todo): Observable<Todo> {
-    return this.http.post<Todo>('https://mast-learn-server.herokuapp.com/api/todos/create', todo)
+    return this.http.post<Todo>(`${this.url}/api/todos/create`, todo)
   }
 
-  getAll(): Observable<Todo[]> {
-    return this.http.get<Todo[]>('https://mast-learn-server.herokuapp.com/api/todos')
+  // getAll(): Observable<Todo[]> {
+  //   return this.http.get<Todo[]>(`${this.url}/api/todos`)
+  // }
+
+  get_all_noparams():Observable<Todo[]>{
+    return this.http.get<Todo[]>(`http://localhost:3000/api/todos`)
+  }
+
+  get_all(pagination, page):Observable<Todo[]>{
+    return this.http.get<Todo[]>(`http://localhost:3000/api/todos?pagination=${pagination}&page=${page}`)
   }
 
   // get all by category
   getAllByCategory(cat: string): Observable<Todo[]> {
-    return this.http.get<Todo[]>(`https://mast-learn-server.herokuapp.com/api/todos/cat/${cat}`)
+    return this.http.get<Todo[]>(`${this.url}/api/todos/cat/${cat}`)
   }
 
   getById(id: string): Observable<Todo> {
-    return this.http.get<Todo>(`https://mast-learn-server.herokuapp.com/api/todos/${id}`)
+    return this.http.get<Todo>(`${this.url}/api/todos/${id}`)
       .pipe(map((todo: Todo) => {
           return {
             ...todo, id
           }
       }))
   }
-//
+  //
   update(todo: Todo): Observable<Todo> {
-    return this.http.put<Todo>(`https://mast-learn-server.herokuapp.com/api/todos/update/${todo.id}`, todo)
+    return this.http.put<Todo>(`${this.url}/api/todos/update/${todo.id}`, todo)
   }
 
   completeTodo(id: string): Observable<Todo> {
-    return this.http.put<Todo>(`https://mast-learn-server.herokuapp.com/api/todos/update/complete/${id}`, {
+    return this.http.put<Todo>(`${this.url}/api/todos/update/complete/${id}`, {
       completed: true
     })
   }
 
   expireTodo(id: string): Observable<Todo> {
-    return this.http.put<Todo>(`https://mast-learn-server.herokuapp.com/api/todos/update/expire/${id}`, {
+    return this.http.put<Todo>(`${this.url}/api/todos/update/expire/${id}`, {
       expired: true
     })
   }
 
   delete(id: string): Observable<void> {
-    return this.http.delete<void>(`https://mast-learn-server.herokuapp.com/api/todos/delete/${id}`)
+    return this.http.delete<void>(`${this.url}/api/todos/delete/${id}`)
   }
 }
